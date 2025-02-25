312fcd9d7212a2db7b80db6f8caef654e096047e
Afsal Ahamed <148963395+afsalahamed2025@users.noreply.github.com>
2025-02-25 11:48:57 +0530

    Add files via upload

---
+++ exception.py
@@ -0,0 +1,42 @@
+from operator import index
+
+try:
+    a=10/0
+    print(a)
+
+except Exception as e:
+    print(e)
+from tkinter.font import names
+ # INDEX STRING LA IROKKA KUDATHU
+try:
+    number = [1,21,5]
+    print(number)
+
+except IndexError as e:
+    print("e")
+
+
+# try:
+#     a=10/0
+#     print(a)
+#
+# except Exception as e:
+#     print(e)
+# from tkinter.font import names
+# NAME ERROR STRING LA IROKALAM
+# try:
+#    print(a)
+#
+# except NameError as e:
+#     print("not a mentiioned")
+
+
+
+from tkinter.font import names
+# nu
+try:
+     name = int(input("type pandra"))
+
+except ValueError as e:
+    print(e)
+
