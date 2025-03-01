e04b6feab028c441932c313efb556a77d728bc3f
Afsal Ahamed <148963395+afsalahamed2025@users.noreply.github.com>
2025-03-01 12:46:29 +0530

    Add files via upload

---
+++ forr.py
@@ -0,0 +1,25 @@
+# for i in "python":
+#     print(i)
+#
+#
+# i=100
+# while(i>=50):
+#     print(i)
+#     i-=1
+
+
+numbers = [50, 20, 30, 5, 70]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+
+
+
+
+
+
+
+
+
+
+
+
